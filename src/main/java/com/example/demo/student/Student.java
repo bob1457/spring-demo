34d8a70810914cd58858a4a226d5b1013a65f3c4
Bob Yuan <bob.yuan@yahoo.com>
2022-09-06 20:21:25 -0700

    Adjust table attribute

--- src/main/java/com/example/demo/student/Student.java
+++ src/main/java/com/example/demo/student/Student.java
@@ -3,6 +3,7 @@ package com.example.demo.student;
 import jakarta.persistence.*;
 
 import java.time.LocalDate;
+import java.time.Period;
 
 @Entity
 @Table
@@ -21,6 +22,8 @@ public class Student {
     private String name;
     private String email;
     private LocalDate dob;
+
+    @Transient
     private Integer age;
 
     public Student(){
@@ -29,23 +32,22 @@ public class Student {
     public Student(Long id,
                    String name,
                    String email,
-                   LocalDate dob,
-                   Integer age) {
+                   LocalDate dob
+                   ) {
         this.id = id;
         this.name = name;
         this.email = email;
         this.dob = dob;
-        this.age = age;
     }
 
     public Student(String name,
                    String email,
-                   LocalDate dob,
-                   Integer age) {
+                   LocalDate dob
+                   ) {
         this.name = name;
         this.email = email;
         this.dob = dob;
-        this.age = age;
+
     }
 
     public Long getId() {
@@ -64,8 +66,9 @@ public class Student {
         return dob;
     }
 
-    public Integer getAge() {
-        return age;
+    public int getAge() {
+
+        return Period.between(this.dob, LocalDate.now()).getYears();
     }
 
     public void setId(Long id) {
--- src/main/java/com/example/demo/student/StudentConfig.java
+++ src/main/java/com/example/demo/student/StudentConfig.java
@@ -19,32 +19,27 @@ public class StudentConfig {
             Student edward = new Student(
                         "Edward",
                         "edy2021@gmail.com",
-                        LocalDate.of(1999, Month.MAY, 25),
-                        23
+                        LocalDate.of(1999, Month.MAY, 25)
                 );
             Student victor =  new Student(
                     "Victor",
                     "vy@gmail.com",
-                    LocalDate.of(1996, Month.NOVEMBER, 6),
-                    26
+                    LocalDate.of(1996, Month.NOVEMBER, 6)
             );
             Student bob = new Student(
                     "Bob",
                     "bob.h.yuan@gmail.com",
-                    LocalDate.of(1957, Month.MAY, 15),
-                    64
+                    LocalDate.of(1957, Month.MAY, 15)
             );
             Student sophia = new Student(
                     "Sophia",
                     "sophiayuan77@gmail.com",
-                    LocalDate.of(2007, Month.JANUARY, 7),
-                    23
+                    LocalDate.of(2007, Month.JANUARY, 7)
             );
             Student michelle = new Student(
                     "Michell",
                     "rentppt@gmail.com",
-                    LocalDate.of(1968, Month.JULY, 22),
-                    54
+                    LocalDate.of(1968, Month.JULY, 22)
             );
 
             studentRepository.saveAll(
